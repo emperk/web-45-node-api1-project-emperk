3ac4904d00c4f7a7e81a46e05561df56d2ebbb0e
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-08-30 17:48:15 -0700

    finished GET /api/users/:id

--- api/server.js
+++ api/server.js
@@ -43,6 +43,25 @@ server.get('/api/users', (req, res) => {
     })
 })
 
+server.get('/api/users/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({
+          message: 'The user with the specified ID does not exist'
+        })
+      }
+      res.json(user)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The user information could not be retrieved',
+        err: err.message,
+        stack: err.stack,
+      })
+    })
+})
+
 // .use()
 
 server.use('*', (req, res) => {

14b4c943d758c8258e893cc52ac76280cfa81237
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-08-30 17:08:03 -0700

    finished get /api/users

--- README.md
+++ README.md
@@ -74,8 +74,8 @@ When the client makes a `POST` request to `/api/users`:
 When the client makes a `GET` request to `/api/users`:
 
 - If there's an error in retrieving the _users_ from the database:
-  - [] respond with HTTP status code `500`.
-  - [] return the following JSON object: `{ message: "The users information could not be retrieved" }`.
+  - [X] respond with HTTP status code `500`.
+  - [X] return the following JSON object: `{ message: "The users information could not be retrieved" }`.
 
 When the client makes a `GET` request to `/api/users/:id`:
 
--- api/server.js
+++ api/server.js
@@ -1,11 +1,30 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
 const server = express();
 server.use(express.json());
 
-//.post()
+// .post()
 
+server.post('/api/users', (req, res) => {
 
+})
+
+// .get()
+
+server.get('/api/users', (req, res) => {
+  User.find()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'The users information could not be retrieved',
+        err: err.message,
+        stack: err.stack,
+      })
+    })
+})
 
 // .use()
 
